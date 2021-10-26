7aecc31139f410f6f6993f978858346840117a03
yunhaDevGit <yunhaDev98@gmail.com>
2021-10-26 11:01:26 +0900

    Kafka basic produce, consume test

---
+++ src/main/java/com/example/kafkapubsubtest/KafkaPubSubTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkapubsubtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaPubSubTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KafkaPubSubTestApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/kafkapubsubtest/KafkaPubSubTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkapubsubtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPubSubTestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
